ad3ad495c339830df40858d34bc6dca6e96f5ce2
Bagas <priambudibagaskara@gmail.com>
2022-09-17 12:54:44 +0700

    init

---
+++ map.js
@@ -0,0 +1,2 @@
+const a = [1,2,3]
+console.log(a.map((value, index) => value * 2))
--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  const age = 10;
+  const progress = 100;
+  const appClassname = 'App hehe'
+  const name = `Bagaskara berumur ${age} tahun`;
+  const list = ['ubuntu', 'windows', 'macos', 'manjaro', 'android']
+  const employees = [
+    {
+      name: "bagas",
+      age: 10
+    },
+    {
+      name: "lintang",
+      age: 11
+    }
+  ]
   return (
-    <div className="App">
+    <div className={appClassname}>
       <header className="App-header">
+        <h1>{name}</h1>
+        <div className="progress" style={{ width: '80%' }}>
+          <div className="progress-bar" role="progressbar" aria-label="Example with label" style={{ width: `${progress}%` }} aria-valuenow={progress} aria-valuemin="0" aria-valuemax="100">{progress}%</div>
+        </div>
+        <ul className="list-group">
+          {
+            employees.map((value, index) => <li className="list-group-item">{value.name} berumur {value.age}</li>)
+          }
+        </ul>
+        <ul className="list-group">
+          {
+            list.map((value, index) => <li className="list-group-item">{value}</li>)
+          }
+        </ul>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
