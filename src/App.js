661f43add54d428a7eb7da70aeb6fe7dad4f9b7d
Bagas <priambudibagaskara@gmail.com>
2022-09-22 00:04:23 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,60 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-import Age from './Age'
-import { useEffect, useState } from 'react';
-import Progress from './Progress';
-import CatFact from './CatFact';
-
-const list = ['ubuntu', 'windows', 'macos', 'manjaro', 'android']
-const employees = [
-  {
-    name: "bagas",
-    age: 10
-  },
-  {
-    name: "lintang",
-    age: 11
-  }
-]
-const appClassname = 'App hehe'
 
 function App() {
-  const [counter, setCounter] = useState(0)
-  useEffect(() => {
-    // biasanya dipake buat call API, 1x aja di awal.
-    console.log('useeffect 2')
-  }, [])
   return (
-    <div className={appClassname}>
-      <header className="App-header">
-        <CatFact />
-        <Age counter={counter} setCounter={setCounter} />
-        <Progress progress={counter} />
-        <Progress progress={100} />
-        <ul className="list-group">
-          {
-            employees.map((value, index) => <li className="list-group-item">{value.name} berumur {value.age}</li>)
-          }
-        </ul>
-        <ul className="list-group">
-          {
-            list.map((value, index) => <li className="list-group-item">{value}</li>)
-          }
-        </ul>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      App
+    </header>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Outlet, } from "react-router-dom";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <>
+      <p>Index</p>
+      <Outlet />
+    </>,
+    errorElement: <p>Page Not Found</p>,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/login",
+        element: <p>Login</p>,
+      }
+    ],
+  },
+  {
+    path: "/login2",
+    element: <p>Login2</p>,
+  }
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <RouterProvider router={router} />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
