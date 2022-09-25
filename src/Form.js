af7e8aa9bcc7c4b21985c7fb748156b94e3205d6
Bagas <priambudibagaskara@gmail.com>
2022-09-25 11:29:44 +0700

    auth

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
+import axios from 'axios';
+import { API_KEY } from './constants';
+
+async function generateRequestToken() {
+  // async/await
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `https://api.themoviedb.org/3/authentication/token/new?api_key=${API_KEY}`,
+    })
+    return response.data.request_token
+  } catch (e) {
+    console.error(e)
+    return null
+  }
+}
 
 const Form = () => {
   const formik = useFormik({
@@ -27,35 +43,56 @@ const Form = () => {
       // alert(JSON.stringify(values, null, 2));
     },
   });
-  return <form onSubmit={formik.handleSubmit}>
-    <label htmlFor="username">Username</label>
-    <input
-      id="username"
-      name="username"
-      type="text"
-      onChange={formik.handleChange}
-      onBlur={formik.handleBlur}
-      value={formik.values.username}
-    />
-    {formik.touched.username && formik.errors.username ? (
-      <div>{formik.errors.username}</div>
-    ) : null}
-    <br></br>
-    <label htmlFor="password">Password</label>
-    <input
-      id="password"
-      name="password"
-      type="password"
-      onChange={formik.handleChange}
-      onBlur={formik.handleBlur}
-      value={formik.values.password}
-    />
-    {formik.touched.password && formik.errors.password ? (
-      <div>{formik.errors.password}</div>
-    ) : null}
-    <br></br>
-    <button type="submit">Submit</button>
-  </form>
+
+  const generateSessionID = () => {
+    // axios({
+    //   method: 'post',
+    //   url: `https://api.themoviedb.org/3/authentication/session/new?api_key=${API_KEY}`,
+    //   data: {
+    //     // request_token: 
+    //   }
+    // })
+  }
+
+  useEffect(() => {
+    // promise
+    generateRequestToken()
+      .then(requestToken => {
+        window.open("https://www.themoviedb.org/authenticate/" + requestToken + '?redirect_to=https://dibimbing.id/', "_blank")
+      });
+  }, [])
+
+  return <div>
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="username">Username</label>
+      <input
+        id="username"
+        name="username"
+        type="text"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.username}
+      />
+      {formik.touched.username && formik.errors.username ? (
+        <div>{formik.errors.username}</div>
+      ) : null}
+      <br></br>
+      <label htmlFor="password">Password</label>
+      <input
+        id="password"
+        name="password"
+        type="password"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.password}
+      />
+      {formik.touched.password && formik.errors.password ? (
+        <div>{formik.errors.password}</div>
+      ) : null}
+      <br></br>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
 }
 
 export default Form;
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const API_KEY = "e37c79c53f9d7b138c1790e896ed6cb1"
